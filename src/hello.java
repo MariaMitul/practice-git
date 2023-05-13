1f7a9992380f6ffe0fe126e701fa6441fb01c16c
Maria Mitul <mitulmaria@gmail.com>
2023-05-13 20:26:35 +0300

    adding the first .java file

--- src/hello.java
+++ src/hello.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class hello {
+public class hello {
+    public static void main(String[] args) {
+        String name = "Maria";
+
+        System.out.println("Hello " + name + " !");
+    }
 }
